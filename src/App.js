4e2acf7b6f0a33161494867eacc0804f4ecfdeeb
swordbill <swordbillz@gmali.com>
2024-03-20 15:55:28 +0900

    fifth commit

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ class App extends Component {
                         fontWeight: "bolder",
                     }}
                 >
-                    ➡️ TODO LIST
+                    ➡️ NEW TODO LIST
                 </Row>
 
                 <hr />
